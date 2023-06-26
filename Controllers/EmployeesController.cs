3ef7dff6ec8c5c92e8869be9ad769ed8bb73818f
gur4hc <gur4hc@bosch.com>
2023-06-26 16:47:14 +0700

    Create Employee API Mock Data

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bogus;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web.Resource;
+using webapi.Models;
+
+namespace webapi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ILogger<EmployeesController> _logger;
+
+        // The Web API will only accept tokens 1) for users, and 2) having the "access_as_user" scope for this API
+        static readonly string[] scopeRequiredByApi = new string[] { "Employee.R" };
+
+        public EmployeesController(ILogger<EmployeesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
+
+            Random rnd = new Random();
+            var testEmployees = new Faker<Employee>()
+                .StrictMode(true)
+                .RuleFor(o => o.Ntid, f => f.UniqueIndex.ToString())
+                .RuleFor(o => o.DisplayName, f => f.Name.FullName())
+                .RuleFor(o => o.Email, f => f.Internet.Email());
+
+            return testEmployees.Generate(rnd.Next(1000)).ToList();
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace webapi.Models
+{
+    public class Employee
+    {
+        public string Ntid { get; set; }
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+    }
+}
